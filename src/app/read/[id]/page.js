ec0fa9bf1ca8a0e3a3c4b1de711caddb9d3d4cc1
THCode <qnprk55@gmail.com>
2024-07-31 23:57:00 +0900

    cc

---
+++ src/app/read/[id]/page.js
@@ -0,0 +1,10 @@
+export default async function Read(props) {
+  const response = await fetch(`${[process.env.NEXT_PUBLIC_API_URL]}topics/${props.params.id}`, {cache : 'no-store'});
+  const topic = await response.json();
+  return(
+    <>
+      <h2>{topic.title}</h2>
+      {topic.body}ㅋㅋ
+    </>
+  )
+}
